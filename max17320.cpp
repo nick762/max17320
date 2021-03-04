7199ed541f59fd639bf3b1f96ee41a39e5b22234
nick762 <nzhdanov@gmail.com>
2021-03-04 11:28:45 +0300

    Библиотека для MAX17320
    Signed-off-by: nick762 <nzhdanov@gmail.com>

---
+++ max17320.cpp
@@ -0,0 +1,217 @@
+#include "max17320.h"
+#include <OneWire.h>
+#include "Arduino.h"
+
+OneWire _max320(A0);
+
+
+uint8_t MAX17320::_scan(){
+	uint8_t addr[8];
+	_max320.reset_search();
+	if(!_max320.search(addr)){
+		_max320.reset_search();
+		return 0;
+	}else{
+		return 1;
+	}
+}			
+
+uint8_t MAX17320::_cellNum(){
+  /*boolean present;
+  uint8_t vol; 
+  present = _max320.reset();
+  _max320.skip();
+  if(present == true){        
+    _max320.write(0x69);
+    _max320.write(0xb5);
+    _max320.write(0x01);    
+    vol = _max320.read();
+    vol = vol<<3;
+    if(vol <= 16){
+		return 0;
+    }else{
+		return 1;
+    }
+  }else{
+    return 2;
+  }*/
+    uint8_t num = lowByte(_readReg(0xb5, 0x01));
+	num = num<<6;
+	//Serial.println(type,HEX);
+	return num;
+}	
+		
+uint16_t MAX17320::_readReg(uint8_t lsb, uint8_t msb)
+{
+		boolean present;
+		uint16_t vol;
+		present = _max320.reset();
+		_max320.skip();
+		if(present){
+			_max320.write(0x69);
+			_max320.write(lsb);
+			_max320.write(msb);
+			vol = _max320.read();
+			vol |= _max320.read()<<8;
+			return vol;
+		}else{
+			return 0;
+		}
+}
+
+void MAX17320::_writeData(uint8_t lsb, uint8_t msb, uint8_t* data){
+	boolean present;
+	present = _max320.reset();
+	if(present){
+		_max320.write(0x6c);
+		_max320.write(lsb);
+		_max320.write(msb,1);
+		_max320.write(data[0]);
+		_max320.write(data[1]);
+		delay(25);
+		//_max320.write(data[i],1);
+		//for(int i=0; i<8; i++){
+		//	_max320.write(data[i],1);
+		//}
+	}
+}
+
+uint8_t MAX17320::_readType()
+{	
+	uint8_t type = lowByte(_readReg(REG_TYPE_320, REG_MSB_320));
+	type = type<<4;
+	//Serial.println(type,HEX);
+	return type;
+}
+
+uint16_t MAX17320::_readCapacity(){
+		uint16_t capacity = _readReg(REG_CAPACITY_320, REG_MSB_320);
+		return capacity / 256; 
+}
+	
+uint16_t MAX17320::_readFCap(){
+	uint16_t capacity = (int16_t)_readReg(REG_FCAP_320, REG_MSB_320);
+	uint16_t rSense = _readReg(0xcf, 0x01);
+	if(rSense == 500){		
+		return capacity * 1.0F;
+	}else if(rSense == 1000){
+		return capacity * 0.5F;
+	}else if(rSense == 2000){
+		return capacity * 0.25F;
+	}else{
+		return -1;		
+	}
+}
+	
+uint16_t MAX17320::_readVoltage(){
+		uint16_t voltage = _readReg(REG_VOLTAGE_320, REG_MSB_320);
+		//return voltage * 1.25F;
+		return voltage * 0.3125F;
+}
+
+int MAX17320::_readCurrent(){
+	uint16_t rSense = _readReg(0xcf, 0x01);
+	int current = (int16_t)_readReg(REG_CURRENT_320, REG_MSB_320);
+	if(rSense == 500){		
+		return current * 31.25F / 100;
+	}else if(rSense == 1000){
+		return current * 15.625F / 100;
+	}else if(rSense == 2000){
+		return current * 7.8125F / 100;
+	}else{
+		return -1;		
+	}
+}
+
+int MAX17320::_readTemperature(){
+	int temperature = (int16_t)_readReg(REG_TEMPERATURE_320, REG_MSB_320);
+	return temperature / 256;
+}
+
+uint16_t MAX17320::_readCell1(){
+	uint16_t cell = _readReg(REG_C1V_320 ,REG_MSB_320);
+	return cell * 0.078125F;
+}
+
+uint16_t MAX17320::_readCell2(){
+	uint16_t cell = _readReg(REG_C2V_320 ,REG_MSB_320);
+	return cell * 0.078125F;
+}
+
+uint16_t MAX17320::_readCell3(){
+	uint16_t cell = _readReg(REG_C3V_320 ,REG_MSB_320);
+	return cell * 0.078125F;
+}
+
+uint16_t MAX17320::_readCell4(){
+	uint16_t cell = _readReg(REG_C4V_320 ,REG_MSB_320);
+	return cell * 0.078125F;
+}
+
+uint8_t MAX17320::_readSerial(){
+	/*char *str;
+	uint16_t sn;
+	for(int i=0; i<6; i++){
+		sn = _readReg(0xd8+i, 0x01);
+		sn |= _readReg(0xd8+i, 0x01)<<8;
+		*str++ = sn;
+	}	
+	return str;*/
+	uint8_t addr[8];
+	_max320.reset_search();
+	if(!_max320.search(addr)){
+		_max320.reset_search();
+		return 0;
+	}else{
+		//for(int i=0; i<8; i++);
+		return addr[7];
+	}
+}
+
+String MAX17320::GetData()
+{	
+	String comm = ",";
+	String data = "";
+	uint16_t voltage,c1v,c2v,c3v,c4v,fCap,capacity;
+	uint8_t ser;
+	int temperature, current, stat, date;
+	
+	voltage = _readVoltage();
+	current = _readCurrent();
+	capacity = _readCapacity();
+	fCap = _readFCap();
+	temperature = _readTemperature();
+	ser = _readSerial();
+	stat = 0;
+	date = 0;
+	c1v = _readCell1();
+	c2v = _readCell2();
+	c3v = _readCell3();
+	c4v = _readCell4();
+	//c3v = 0;
+	//c4v = 0;
+	
+	/*voltage = 16320;
+	current = 340;
+	capacity = 75;
+	fCap = 4570;
+	temperature = 22.10;
+	ser = 777;
+	stat = 1;
+	date = 1598610766;
+	c1v = 4100;
+	c2v = 3952;
+	c3v = 4023;
+	c4v = 4098;	*/
+	
+	//			0			  1				  2				 3			 4		 5		   6        7        8        9         10        11
+	
+	//{				
+		data = voltage+comm+current+comm+temperature+comm+capacity+comm+c1v+comm+c2v+comm+c3v+comm+c4v+comm+ser+comm+stat+comm+date+comm+fCap;
+	//}
+	//{
+		//data = voltage+comm+current+comm+temperature+comm+capacity+comm+c1v+comm+c2v+comm+c3v+comm+c4v+comm+ser+comm+stat+comm+date;
+	//}
+	//delay(1000);
+	return data;
+}
---
+++ max17320.h
@@ -0,0 +1,41 @@
+#ifndef MAX17320_h
+#define MAX17320_h
+
+#include <OneWire.h>
+
+#define REG_MSB_320				0x00
+#define REG_CAPACITY_320				0x06
+#define REG_VOLTAGE_320				0xda
+#define REG_CURRENT_320				0x1c
+#define REG_TEMPERATURE_320			0x1b
+#define	REG_C1V_320					0xd8
+#define	REG_C2V_320					0xd7
+#define	REG_C3V_320					0xd6
+#define	REG_C4V_320					0xd5
+#define	REG_FCAP_320					0x05
+#define REG_TYPE_320					0x21
+
+class MAX17320{
+	public:
+		uint16_t _readCapacity();
+		uint16_t _readVoltage();
+		uint16_t _readCell1();
+		uint16_t _readCell2();
+		uint16_t _readCell3();
+		uint16_t _readCell4();
+		uint16_t _readFCap();
+		uint8_t _readType();
+		int _readCurrent();
+		int _readTemperature();
+		String GetData(void);
+		uint8_t _scan();
+		void _writeData(uint8_t, uint8_t, uint8_t*);
+		uint8_t _readSerial();
+		uint8_t _cellNum();
+	private:
+		uint16_t _readReg(uint8_t, uint8_t);
+};
+
+#endif
+		
+	
